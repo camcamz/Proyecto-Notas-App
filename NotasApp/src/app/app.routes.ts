5e83a8013d1cdbddf15b1757a8a1fd23bb9e73b9
Camila <czarza@fi.uba.ar>
2024-11-03 18:55:27 -0300

    Inicializacion del proyecto, cree el componente landing para cuando apenas se levanta el local host

---
+++ NotasApp/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { LandingComponent } from './componentes/landing/landing.component'; // Asegúrate de que esta ruta sea correcta
+
+export const routes: Routes = [
+    { path: '', component: LandingComponent }
+];
